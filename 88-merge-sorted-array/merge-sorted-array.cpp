9e36d15edbbc0acd5fac7cf909a4afc73b35fa1a
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2025-06-11 09:38:45 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (38.88%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -20,3 +20,10 @@ public:
         }
 };
     
+
+
+// sbse pehle to LAST name k variable m pehli array ka last wala index dal do 
+// uske bad jbhtk m or n dono m se koi bhi 0 se bda hoga tbtk loop chlta rhega 
+// fr if condition lga k dono m wale index or n wale index m se usko last m lga do 
+// fr jis bhi index ki value ati hai use ek km krdo fr last ki bhi krnni he pdegi 
+// jb last m ek case ayega jisme n ki value ho skta h m se jada ho to unko bhi last m dal kr n or last dono km krdo
