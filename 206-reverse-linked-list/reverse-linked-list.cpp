7275509703326473146b8ee198abb34445d7074d
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2025-06-17 18:54:38 -0700

    Time: 0 ms (100.00%) | Memory: 13.5 MB (40.09%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,21 +1,37 @@
 class Solution{
     public:
-    ListNode* reverseList(ListNode* &prev , ListNode* &curr){
-        if(curr==NULL){
-            return prev ;
+    // Recursive method
+    // ListNode* reverseList(ListNode* &prev , ListNode* &curr){
+    //     if(curr==NULL){
+    //         return prev ;
+    //     }
+
+    //     ListNode* forward = curr->next;
+    //     curr->next=prev;
+    //     prev=curr;
+    //     curr=forward;
+    //     return reverseList(prev , curr);
+    // }
+    // ListNode* reverseList(ListNode *head){
+    //     ListNode* prev=NULL;
+    //     ListNode* curr=head;
+
+    //     ListNode* newHead = reverseList(prev,curr);
+    //     return newHead;
+    // }
+
+
+    // Loop Method 
+       ListNode* reverseList(ListNode* head){
+        ListNode* prev= NULL;
+        ListNode* curr= head;
+
+        while(curr!=NULL){
+            ListNode* forward = curr->next;
+            curr->next=  prev;
+            prev=curr;
+            curr=forward;
         }
-
-        ListNode* forward = curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=forward;
-        return reverseList(prev , curr);
-    }
-    ListNode* reverseList(ListNode *head){
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-
-        ListNode* newHead = reverseList(prev,curr);
-        return newHead;
-    }
+        return prev;
+       }
 };
