195cefa80db38e45728ba5c1f8d74059bdaaa53a
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-02-23 11:17:08 -0800

    Time: 6 ms (8.70%) | Memory: 77 MB (74.33%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,27 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* iterative(ListNode* l1 , ListNode* l2){
-        ListNode * ans = new ListNode(-1)  ;
-        ListNode * it = ans ;   
-        if(!l1 || !l2 ) return l1?l1:l2 ;
-        int carry = 0 ;
-        while(l1 || l2 ||carry ){
+    // ListNode* iterative(ListNode* l1 , ListNode* l2){
+    //     ListNode * ans = new ListNode(-1)  ;
+    //     ListNode * it = ans ;   
+    //     if(!l1 || !l2 ) return l1?l1:l2 ;
+    //     int carry = 0 ;
+    //     while(l1 || l2 ||carry ){
+    //     int a = l1 ? l1->val : 0 ;
+    //     int b = l2 ? l2->val : 0 ;
+    //     int sum = a+b+carry ;
+    //     int digit = sum % 10;
+    //     carry = sum/10 ;
+    //     it->next = new ListNode(digit);
+    //     it= it->next ;
+    //     l1 = l1?l1->next:NULL ;
+    //     l2 = l2?l2->next:NULL ;
+    //     }
+    //     ListNode * final = ans->next;
+    //     delete ans;
+    //     return final ;
+    // }
+ListNode* recursive(ListNode* l1 , ListNode* l2 ,int carry=0 ){
+          
+        if(!l1 && !l2 && !carry) return 0 ;
+       
         int a = l1 ? l1->val : 0 ;
         int b = l2 ? l2->val : 0 ;
         int sum = a+b+carry ;
         int digit = sum % 10;
         carry = sum/10 ;
-        it->next = new ListNode(digit);
-        it= it->next ;
-        l1 = l1?l1->next:NULL ;
-        l2 = l2?l2->next:NULL ;
-        }
-        ListNode * final = ans->next;
-        delete ans;
-        return final ;
-    }
+        ListNode*ans = new ListNode(digit);
+        ans->next = recursive(l1?l1->next:l1 , l2?l2->next:l2  , carry);
+        return ans;
+}
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return iterative(l1 , l2);
+        // return iterative(l1 , l2);
+        return recursive(l1 , l2);
     }
 };
