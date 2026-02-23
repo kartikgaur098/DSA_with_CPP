cc1e0688deb0149edbb8541c21c3ddb3b6b73fd5
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-02-23 09:26:36 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (98.88%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,38 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL)
-            return l2;
-        if (l2 == NULL)
-            return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+        return list2;
+        }
+        if(list2 == NULL){
+        return list1;
+        }
+        ListNode*ans = new ListNode(-1);
+        ListNode*ptr = ans ;
 
-        ListNode* ans = new ListNode(-1); // Dummy node
-        ListNode* mptr = ans;
-
-        while (l1 && l2) {
-            if (l1->val <= l2->val) {
-                mptr->next = l1;
-                l1 = l1->next;
-            } else {
-                mptr->next = l2;
-                l2 = l2->next;
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                ptr->next = list1;
+                ptr = list1;
+                list1 = list1->next;
             }
-            mptr = mptr->next;
+            else{
+                ptr->next = list2 ;
+                ptr = list2 ;
+                list2 = list2->next ;
+            }
+            if(list1){
+                ptr->next=list1;
+            }
+            if(list2){
+                ptr->next=list2;
+            }
         }
-
-        while (l1) {
-            mptr->next = l1;
-            l1 = l1->next;
-            mptr = mptr->next;
-        }
-
-        while (l2) {
-            mptr->next = l2;
-            l2 = l2->next;
-            mptr = mptr->next;
-        }
-
-        return ans->next;
+            return ans->next;
     }
 };
