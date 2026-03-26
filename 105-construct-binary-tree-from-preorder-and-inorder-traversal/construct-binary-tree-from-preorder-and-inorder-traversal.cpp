e1d9c1a770fbcb1543f40a54beb7eea5d5acb55d
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-03-26 07:18:58 +0530

    Time: 3 ms (75.56%) | Memory: 27.2 MB (81.59%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,48 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
 
-    void createMapping(unordered_map<int , int> &valueMapping,vector<int> inorder){
-        for(int i=0 ; i<inorder.size() ; i++){
-            int element = inorder[i];
-            int index = i;
-            valueMapping[element]=index;
+    int getIndex(vector<int>& inorder, int element){
+        for(int i = 0; i < inorder.size(); i++){
+            if(element == inorder[i]){
+                return i;
+            }
         }
+        return -1;
     }
-    TreeNode* constructTree(unordered_map<int , int> &valueMapping,vector<int>& preorder,vector<int>& inorder,int &preIndex, int inorderStart ,int inorderEnd, int size){
-        if(preIndex >= size){    
-            return NULL;
-        }
-        if(inorderStart>inorderEnd){
-            return NULL;
-        }
-        int element = preorder[preIndex];
-        preIndex++;
-        TreeNode* root = new TreeNode(element);
-        int elementIndexInsideInorder = valueMapping[element];
 
-        root->left= constructTree(valueMapping,preorder , inorder,preIndex, inorderStart , elementIndexInsideInorder-1,size);
-        root-> right= constructTree(valueMapping,preorder , inorder,preIndex, elementIndexInsideInorder+1 , inorderEnd,size);
+    TreeNode* constructTree(vector<int>& preorder, vector<int>& inorder,
+                            int inorderStart, int inorderEnd,
+                            int &preorderIndex, int size){
+
+        if(inorderStart > inorderEnd || preorderIndex >= size){
+            return NULL;
+        }
+
+        int element = preorder[preorderIndex++];
+        TreeNode* root = new TreeNode(element);
+
+        int index = getIndex(inorder, element);
+
+        root->left = constructTree(preorder, inorder, inorderStart, index - 1, preorderIndex, size);
+        root->right = constructTree(preorder, inorder, index + 1, inorderEnd, preorderIndex, size);
+
         return root;
     }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int preIndex =0;
         int inorderStart = 0;
-        int inorderEnd= inorder.size()-1;
-        int size= inorder.size();
-        unordered_map<int , int> valueMapping;
-        createMapping(valueMapping, inorder);
-        TreeNode* root = constructTree(valueMapping,preorder , inorder,preIndex, inorderStart , inorderEnd,size);
-        return root;        
+        int inorderEnd = inorder.size() - 1;
+        int preorderIndex = 0;
+        int size = inorder.size();
+
+        return constructTree(preorder, inorder, inorderStart, inorderEnd, preorderIndex, size);
     }
 };
