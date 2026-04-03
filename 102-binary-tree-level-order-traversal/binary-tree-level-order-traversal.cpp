aa0b33c29e34ce56e7b9b6c5cf8161a524281722
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-03 08:03:56 +0530

    Time: 3 ms (27.98%) | Memory: 17.1 MB (44.02%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,28 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == NULL)
-            return result;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> ans(size);
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* front = q.front();
-                q.pop();
-
-                ans[i] = front->val;
-                if (front->left)
-                    q.push(front->left);
-                if (front->right)
-                    q.push(front->right);
-            }
-            result.push_back(ans);
+        vector<vector<int>> ans ;
+        if(root == NULL){
+            return ans;
         }
-        return result;
+        queue<TreeNode*> q ;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+                 vector<int> path ;
+            for(int i = 0 ; i < size ; i++){
+                TreeNode* element = q.front() ;
+                q.pop();
+                path.push_back(element->val);
+
+                if(element->left != NULL)
+                q.push(element->left);
+
+                if(element->right != NULL)
+                q.push(element->right);
+            }
+            ans.push_back(path);
+        } 
+
+        return ans ;
     }
-}
-;
+};
