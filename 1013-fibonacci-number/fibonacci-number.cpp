49c9884b35c3356482d7a1c0d9d5e89e41dab965
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-14 06:32:24 +0530

    Time: 2 ms (78.45%) | Memory: 8.4 MB (10.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
-    int fib(int n) {
+    // int fibRec(int n) {
+    //    if(n == 0) return 0 ;
+    //    if(n == 1) return 1 ;
+
+    //    int ans = fibRec(n-1) + fibRec(n-2);
+    //    return ans ;
+    // }
+
+      int fibusingMemoization(int n , vector<int> &dp) {
+
+        // Memoization steps -
+    // step 1: isme sbse pehle check krte h 1D , 2D , 3D konsi DP lgegi ye krenge hm jitne variables change rhe h uske base pr 
+    // step 2: iske bad vector ko pass krenge funtion m or store krayenge vector m he 
+    // step 3: base condition k bad check krenge kya ans already pda h array k andr agr hn to return krdenge
+
        if(n == 0) return 0 ;
        if(n == 1) return 1 ;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+       dp[n] = fibusingMemoization(n-1 , dp) + fibusingMemoization(n-2, dp);
+       return dp[n] ;
+    }
 
-       int ans = fib(n-1) + fib(n-2);
+    int fib(int n) {
+        //  memoization 
+       vector<int> dp(n+1, -1);
+       int ans = fibusingMemoization(n , dp);
        return ans ;
     }
 };
