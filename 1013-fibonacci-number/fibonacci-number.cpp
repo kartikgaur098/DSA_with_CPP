e2674108e95fe17d12803aac1c017af385eb0fae
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-14 06:54:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    // int fibRec(int n) {
-    //    if(n == 0) return 0 ;
-    //    if(n == 1) return 1 ;
+    int fibRec(int n) {
+       if(n == 0) return 0 ;
+       if(n == 1) return 1 ;
 
-    //    int ans = fibRec(n-1) + fibRec(n-2);
-    //    return ans ;
-    // }
+       int ans = fibRec(n-1) + fibRec(n-2);
+       return ans ;
+    }
 
       int fibusingMemoization(int n , vector<int> &dp) {
+        // Pure recursive apporach ;
 
         // Memoization steps -
     // step 1: isme sbse pehle check krte h 1D , 2D , 3D konsi DP lgegi ye krenge hm jitne variables change rhe h uske base pr 
@@ -24,10 +25,31 @@ public:
        return dp[n] ;
     }
 
+
+    int fibusingTabulation(int n){
+        // Pure iterative apporach ;
+        // create a dp array 
+        vector<int> dp(n+1 , -1);
+
+        // check base in recusrsion and update array
+        dp[0] = 0 ;
+        if(n == 0 ) return 0 ;
+        dp[1] = 1 ;
+
+        // check parameter reverse it and run a loop
+         for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+         }
+
+        return dp[n];
+    }
     int fib(int n) {
         //  memoization 
-       vector<int> dp(n+1, -1);
-       int ans = fibusingMemoization(n , dp);
-       return ans ;
+    //    vector<int> dp(n+1, -1);
+    //    int ans = fibusingMemoization(n , dp);
+    //    return ans ;
+
+        // Tabulation
+        return fibusingTabulation(n);
     }
 };
