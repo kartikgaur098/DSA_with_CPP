8a18ca96263ce9baa808038c0d0ee08de99c72e7
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-22 22:28:20 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (69.71%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,48 +1,12 @@
 class Solution {
 public:
-    // int missingNumber(vector<int>& nums) {
-    //     sort(nums.begin(), nums.end());  // O(n log n)
-    //     int start = 0, end = nums.size() - 1;
-
-    //     while (start <= end) {
-    //         int mid = start + (end - start) / 2;
-
-    //         if (nums[mid] > mid) {
-    //             end = mid - 1;
-    //         } else {
-    //             start = mid + 1;
-    //         }
-    //     }
-
-    //     return start;
-    // }
-
-    // sum wala solution 
-    // int missingNumber(vector<int>& nums) {
-    //     int ans=0;
-    //      int sum=0,sumIndex=0;
-    //      int n=nums.size();
-    //     for (int i =0 ; i<n ; i++){
-    //         sum = sum+ nums[i];
-    //         sumIndex = sumIndex + i;
-    //     }
-    //     ans = (sumIndex + n) - sum ;
-    //     return ans;
-    // }
-
-    // xor wala sol
     int missingNumber(vector<int>& nums) {
-    int xorAll = 0;
-    int n = nums.size();
-
-    for (int i = 0; i < n; i++) {
-        xorAll ^= i;         // XOR of all indices
-        xorAll ^= nums[i];   // XOR of all array elements
+        int n = nums.size();
+        int expected = n * (n+1)/2;
+        int ans = 0 ;
+       for(int i = 0 ; i<n ; i++){
+        ans = ans +nums[i];
+       }
+        return (expected - ans) ;
     }
-
-    xorAll ^= n;             // Include the last number (n)
-
-    return xorAll;           // Result is the missing number
-}
-
 };
