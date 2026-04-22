adecc80f1e05243af05f3b466e77c0dc7bc1b4c6
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-22 23:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (83.77%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,21 +1,16 @@
 class Solution {
-    public:
-    int removeElement(vector <int> &nums , int val){
-       int  validElementKaIndex=0;
-       int j = nums.size();
-        for(int i=0;i<j;i++){
-            if(nums[i]!=val){
-                nums[validElementKaIndex]=nums[i];
-                validElementKaIndex++;
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count  = 0 ;
+        int j = 0 ;
+
+        for(int i = 0 ; i<nums.size() ;i++){
+            if(nums[i] != val){
+                count++;
+                swap(nums[i] , nums[j]);
+                j++ ;
             }
         }
-        return validElementKaIndex;
+        return count ;
     }
 };
-
-
-
-// ques ko chahiye inplace value ko remove krdein order maintain krne ki jrurat  nhi hai or remove k bad kitne element bche unka number chaiye
-
-// ans m ye soch skte hain ki koi nya variable lelo jisme valid number ka index ajae 
-// fr ek chhla lo loop last tk jisme condition check krlo ki number shi h ya  nhi agr shi number hua to us number ko nye wale index pr put krlo or index bdha do 
