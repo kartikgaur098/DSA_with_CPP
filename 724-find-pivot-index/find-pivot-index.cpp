70b65e11b46d9500c1c0663a06f6cb4371eddebb
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-22 22:55:29 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (78.20%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int total = 0, left = 0;
-        for (int num : nums) total += num;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (left == total - left - nums[i]) return i;
-            left += nums[i];
+        int tsum = 0 ;
+        
+        for(int i = 0 ; i<nums.size() ; i++){
+            tsum = tsum + nums[i];
         }
 
-        return -1;
+        int lsum= 0 ;
+        for(int i = 0 ; i<nums.size() ;i++){
+           
+            tsum = tsum-nums[i] ;
+            if(lsum == tsum){
+               return i ;
+            }
+             lsum = lsum + nums[i];
+        }
+        return -1 ;
     }
 };
