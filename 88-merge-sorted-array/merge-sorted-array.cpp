b09d4a31d14a6494e080974bac8503563c9dbe37
kartikgaur098 <168162996+kartikgaur098@users.noreply.github.com>
2026-04-27 08:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.52%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-        void merge(vector<int>& nums1 ,int m , vector<int>& nums2 , int n) {
-            int last = m+n-1;
-            while (m>0 && n>0){
-                if(nums1[m-1]>nums2[n-1]){
-                    nums1[last]=nums1[m-1];
-                    m-=1;
-                }else{
-                    nums1[last]=nums2[n-1];
-                     n-=1;
-                }
-                last-=1;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1 ;
+        int j= n-1 ;
+        int k = m+n -1;
+
+        while( i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i-- ;
+            }else {
+                nums1[k] = nums2[j];
+                j--;           
             }
-            while (n>0){
-                nums1[last]=nums2[n-1];
-                last-=1;
-                n-=1;
-            }   
+            k-- ;
         }
+
+        while(j >= 0 ){
+            nums1[k] = nums2[j];
+            j-- ;
+            k--;
+        }
+    }
 };
-    
-
-
-// sbse pehle to LAST name k variable m pehli array ka last wala index dal do 
-// uske bad jbhtk m or n dono m se koi bhi 0 se bda hoga tbtk loop chlta rhega 
-// fr if condition lga k dono m wale index or n wale index m se usko last m lga do 
-// fr jis bhi index ki value ati hai use ek km krdo fr last ki bhi krnni he pdegi 
-// jb last m ek case ayega jisme n ki value ho skta h m se jada ho to unko bhi last m dal kr n or last dono km krdo
